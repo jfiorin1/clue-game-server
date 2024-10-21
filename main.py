e0f60a87ff5bf28cc05a55c67cf252abdb8b63bb
John Fiorini <jfiorin1@jh.edu>
2024-10-21 08:46:02 -0400

    game state machine stubs on gameManager.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import asyncio
 import json
 from websockets.asyncio.server import serve
 
+from gameManager import gameManager
+
 
 # this will recieve a message and print to the console
 async def handler(websocket):
+
     await websocket.send(json.dumps("Hello from server!"))
+    # set game state to the start screen
     async for message in websocket:
         # handle message appropriately and update game state
         print(message)
+
         # send back a dummy message
         await websocket.send("Message receieved and updated game state")
 
@@ -17,7 +22,6 @@ async def handler(websocket):
 async def main():
     async with serve(handler, "", 3000):
         await asyncio.get_running_loop().create_future()
-
 
 if __name__ == "__main__":
     print("Starting clue-less server!")
