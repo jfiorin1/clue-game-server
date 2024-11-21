6444c7cb88d3778d413647e697352e8728460d13
Nick Weiner <wnicholas114@gmail.com>
2024-11-20 21:27:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,62 +29,62 @@ async def handler(websocket):
 # start server to run forever
 async def main():
     async with serve(handler, "", 3000):
-        dummy = {
-            "message_type": "player_join",
-            "player_name": "xxXXSniperYoMama420Xxx"
-        }
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-        dummy = {
-            "message_type": "player_join",
-            "player_name": "1xxXXSniperYoMama420Xxx"
-        }
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-        dummy = {
-            "message_type": "player_join",
-            "player_name": "2xxXXSniperYoMama420Xxx"
-        }
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-
-        dummy = {
-            "message_type": "player_ready",
-            "player_name": "xxXXSniperYoMama420Xxx"
-        }
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-        dummy = {
-            "message_type": "player_ready",
-            "player_name": "1xxXXSniperYoMama420Xxx"
-        }
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-        dummy = {
-            "message_type": "player_ready",
-            "player_name": "2xxXXSniperYoMama420Xxx"
-        }
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-
-        dummy = {
-            "message_type": "make_claim",
-            "player_name": "xxXXSniperYoMama420Xxx",
-            "is_accused": True,
-            "character": ClueCharacter.MRS_WHITE.value,
-            "weapon": WeaponName.ROPE.value,
-            "room": Room.HALL.value
-        }
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-
-        time.sleep(1)
-
-        dummy["is_accused"] = False
-        dummy_json = json.dumps(dummy)
-        game_manager.parse_message(dummy_json)
-
-        print(game_manager.json_serialize())
+        # dummy = {
+        #     "message_type": "player_join",
+        #     "player_name": "xxXXSniperYoMama420Xxx"
+        # }
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        # dummy = {
+        #     "message_type": "player_join",
+        #     "player_name": "1xxXXSniperYoMama420Xxx"
+        # }
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        # dummy = {
+        #     "message_type": "player_join",
+        #     "player_name": "2xxXXSniperYoMama420Xxx"
+        # }
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        #
+        # dummy = {
+        #     "message_type": "player_ready",
+        #     "player_name": "xxXXSniperYoMama420Xxx"
+        # }
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        # dummy = {
+        #     "message_type": "player_ready",
+        #     "player_name": "1xxXXSniperYoMama420Xxx"
+        # }
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        # dummy = {
+        #     "message_type": "player_ready",
+        #     "player_name": "2xxXXSniperYoMama420Xxx"
+        # }
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        #
+        # dummy = {
+        #     "message_type": "make_claim",
+        #     "player_name": "xxXXSniperYoMama420Xxx",
+        #     "is_accused": True,
+        #     "character": ClueCharacter.MRS_WHITE.value,
+        #     "weapon": WeaponName.ROPE.value,
+        #     "room": Room.HALL.value
+        # }
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        #
+        # time.sleep(1)
+        #
+        # dummy["is_accused"] = False
+        # dummy_json = json.dumps(dummy)
+        # game_manager.parse_message(dummy_json)
+        #
+        # print(game_manager.json_serialize())
         # [print(player.characterHandler.character.value) for player in game_manager.players]
         await asyncio.get_running_loop().create_future()
 
